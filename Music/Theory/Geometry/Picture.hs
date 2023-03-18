2f9b696c74ba09f7b2ad78e8456008063889d8a2
rohan <rd@slavepianos.org>
2023-03-18 13:12:29 +1100

    +Music/Theory/Geometry/Picture

---
+++ Music/Theory/Geometry/Picture.hs
@@ -0,0 +1,146 @@
+-- | Simple pictures.
+module Music.Theory.Geometry.Picture where
+
+import Data.List {- base -}
+import Data.Maybe {- base -}
+
+import Music.Theory.Math {- hmt-base -}
+import Music.Theory.Geometry.Vector {- hmt-base -}
+
+-- * Type
+
+type Line_Width r = r
+
+-- | (dash,no-dash)
+type Dash r = ([r],r)
+
+no_dash :: Num r => Dash r
+no_dash = ([],0)
+
+type Rgb t = V3 t
+type Rgba t = V4 t
+type Clr = Rgba Double -- (0, 1)
+
+-- | (line-width,colour,dash-pattern)
+data Pen r = Pen (Line_Width r) Clr (Dash r) deriving (Eq,Show)
+
+-- | (centre,radius)
+type Centre_Radius t = (V2 t,t)
+
+data Mark r =
+  Line (Pen r) (V2 (V2 r))
+  | Polygon (Either (Pen r) Clr) [V2 r]
+  | Circle (Either (Pen r) Clr) (Centre_Radius r)
+  | Dot Clr (Centre_Radius r)
+  deriving (Eq,Show)
+
+type Picture r = [Mark r]
+
+-- * Constructors
+
+line_seq :: Num r => Pen r -> [V2 r] -> [Mark r]
+line_seq pen =
+    let adj l = zip l (tail l)
+    in map (Line pen) . adj
+
+polygon_l :: Pen r -> [V2 r] -> Mark r
+polygon_l pen = Polygon (Left pen)
+
+polygon_f :: Clr -> [V2 r] -> Mark r
+polygon_f clr = Polygon (Right clr)
+
+circle_l :: Pen r -> Centre_Radius r -> Mark r
+circle_l pen = Circle (Left pen)
+
+circle_f :: Clr -> Centre_Radius r -> Mark r
+circle_f clr = Circle (Right clr)
+
+-- * Analysis
+
+circle_bounds :: Num t => V2 t -> t -> V2 (V2 t)
+circle_bounds (x, y) n = ((x - n, y - n), (n * 2, n * 2))
+
+mark_wn :: (Num n, Ord n) => Mark n -> V2 (V2 n)
+mark_wn m =
+    case m of
+      Line _ (p1, p2) -> v2_bounds [p1, p2]
+      Polygon _ p -> v2_bounds p
+      Circle _ (c,r) -> circle_bounds c r
+      Dot _ (c,r) -> circle_bounds c r
+
+mark_normal :: Ord r => Mark r -> Mark r
+mark_normal m =
+    case m of
+      Line p (p1, p2) -> Line p (if p1 <= p2 then (p1, p2) else (p2, p1))
+      Polygon _ _ -> m -- should ensure CCW
+      Circle _ _ -> m
+      Dot _ _ -> m
+
+mark_pt_set :: Mark r -> [V2 r]
+mark_pt_set m =
+    case m of
+      Line _ (p, q) -> [p,q]
+      Polygon _ p -> p
+      Circle _ (p,_) -> [p]
+      Dot _ (p,_) -> [p]
+
+mark_ln :: Mark r -> Maybe (V2 (V2 r))
+mark_ln m =
+    case m of
+      Line _ l -> Just l
+      _ -> Nothing
+
+mark_circle :: Mark r -> Maybe (Centre_Radius r)
+mark_circle m =
+    case m of
+      Circle _ c -> Just c
+      _ -> Nothing
+
+picture_pt_set :: Picture r -> [V2 r]
+picture_pt_set = concatMap mark_pt_set
+
+picture_ln_set :: Picture r -> [V2 (V2 r)]
+picture_ln_set = mapMaybe mark_ln
+
+picture_normalise :: Ord r => Picture r -> Picture r
+picture_normalise = nub . map mark_normal
+
+picture_wn :: (Ord r,Num r) => Picture r -> V2 (V2 r)
+picture_wn = foldl1 v2_bounds_join . map mark_wn
+
+-- * Graph
+
+{- | Extract coloured vertex-sequences from a picture.
+     Dots and circles generate 1-element sequences, lines 2-element sequences, n-polygons generate n+1-element sequences.
+-}
+picture_ln :: Picture t -> [(Clr,[V2 t])]
+picture_ln mk =
+  let get_c x = case x of
+                  Left (Pen _ c _) -> c
+                  Right c -> c
+      f m = case m of
+              Line (Pen _ c _) (p1, p2) -> (c,[p1,p2])
+              Polygon c p -> (get_c c,p ++ [head p])
+              Circle c (p1,_) -> (get_c c,[p1])
+              Dot c (p1,_) -> (c,[p1])
+  in map f mk
+
+{- | Extract graph from set of coloured vertex sequences, ie. 'picture_ln'.
+     Vertices are compared using the given equality function (ie. '~=')
+     Deletes duplicate vertices and edges, which are un-directed.
+-}
+picture_ln_gr :: (Ord n,Ord c) => (n -> n -> Bool) -> [(c,[n])] -> ([(Int,n)],[(V2 Int,c)])
+picture_ln_gr eq_f ln =
+  let v = nubBy eq_f (sort (concatMap snd ln))
+      v_ix x = fromMaybe (error "picture_ln_gr?") (findIndex (eq_f x) v)
+      o (i,j) = (min i j,max i j)
+      adj2 x = zip x (tail x)
+      mk_e (c,p) = map (\(i,j) -> (o (v_ix i,v_ix j),c)) (adj2 p)
+      e = nub (sort (concatMap mk_e ln))
+  in (zip [0..] v,e)
+
+-- | 'picture_ln_gr' of 'picture_ln' of '~='
+picture_gr :: (Floating n,Ord n) => Picture n -> ([(Int,V2 n)],[(V2 Int, Clr)])
+picture_gr =
+  let eq (i,j) (p,q) = i ~= p && j ~= q
+  in picture_ln_gr eq . picture_ln
---
+++ Music/Theory/Geometry/Picture/Svg.hs
@@ -0,0 +1,101 @@
+module Music.Theory.Geometry.Picture.Svg where
+
+import Text.Printf {- base -}
+
+import Music.Theory.Geometry.Matrix {- hmt-base -}
+import Music.Theory.Geometry.Picture {- hmt-base -}
+import Music.Theory.Geometry.Vector {- hmt-base -}
+
+type R = Double
+
+type SvgAttr = (String, String)
+
+clr_to_rgb8 :: Clr -> V3 Int
+clr_to_rgb8 (r, g, b, _) = (round (r * 255),round (g * 255),round (b * 255))
+
+clr_to_hex_string :: Clr -> String
+clr_to_hex_string clr =
+  let (r, g, b) = clr_to_rgb8 clr
+  in printf "#%02x%02x%02x" r g b
+
+clr_to_opacity :: Clr -> Double
+clr_to_opacity (_, _, _, a) = a
+
+fill :: Clr -> [SvgAttr]
+fill clr =
+  [("fill", clr_to_hex_string clr)
+  ,("fill-opacity", show (clr_to_opacity clr))]
+
+stroke :: Pen R -> [SvgAttr]
+stroke (Pen lw clr dash) =
+  concat
+  [[("stroke", clr_to_hex_string clr)
+   ,("stroke-opacity", show (clr_to_opacity clr))
+   ,("stroke-width", show lw)]
+  ,if dash == no_dash
+   then []
+   else [("stroke-dasharray", unwords (map show (fst dash)))
+        ,("stroke-dashoffset", show (snd dash))]]
+
+mark :: Either (Pen R) Clr -> [SvgAttr]
+mark = either stroke fill
+
+type SvgNode = String
+
+data SvgElem = SvgElem SvgNode [SvgAttr] [SvgElem]
+
+line :: Show t => V2 (V2 t) -> [SvgAttr]
+line ((x1, y1), (x2, y2)) = zipWith (,) ["x1", "y1", "x2", "y2"] (map show [x1, y1, x2, y2])
+
+polygon :: Show t => [V2 t] -> [SvgAttr]
+polygon pt =
+  let f (x, y) = show x ++ "," ++ show y
+  in [("points", unwords (map f pt))]
+
+circle :: Show t => Centre_Radius t -> [SvgAttr]
+circle ((x, y), r) = [("cx", show x), ("cy", show y), ("r", show r)]
+
+mark_render :: Mark R -> SvgElem
+mark_render m =
+    case m of
+      Line pen ln -> SvgElem "line" (line ln ++ stroke pen) []
+      Polygon e pt -> SvgElem "polygon" (polygon pt ++ mark e) []
+      Circle e cr -> SvgElem "circle" (circle cr ++ mark e) []
+      Dot clr cr -> SvgElem "circle" (circle cr ++ fill clr) []
+
+translate :: Show a => (a, a) -> String
+translate (x, y) = concat ["translate(", unwords (map show [x, y]), ")"]
+
+matrix :: Show t => (M22 t, V2 t) -> String
+matrix (((a, b), (c, d)), (e, f)) = concat ["matrix(", unwords (map show [a, b, c, d, e, f]), ")"]
+
+transform_group :: Show t => (M22 t, V2 t) -> [SvgElem] -> SvgElem
+transform_group m e = SvgElem "g" [("transform", matrix m)] e
+
+-- | m = margin
+picture_render :: R -> V2 (V2 R) -> Picture R -> SvgElem
+picture_render m w p =
+  let ((x, y), (_, dy)) = w
+      e = (m / 2) - x
+      f = dy + (m / 2) + y
+  in transform_group (((1, 0), (0, -1)), (e, f)) (map mark_render p)
+
+picture_to_svg_elem :: R -> Picture R -> SvgElem
+picture_to_svg_elem m p =
+  let w = picture_wn p
+      (_, (x, y)) = w
+      vb = ("viewbox", unwords (map show [0, 0, m + x,m + y]))
+  in SvgElem "svg" [("xmlns", "http://www.w3.org/2000/svg"), vb] [picture_render m w p]
+
+svg_attr_pp :: SvgAttr -> String
+svg_attr_pp (name, value) = concat [name, "=\"", value, "\""]
+
+svg_elem_pp :: SvgElem -> String
+svg_elem_pp (SvgElem name attr children) =
+  concat
+  ["<", name, unwords (map svg_attr_pp attr), if null children then "/>" else ">"
+  ,unwords (map svg_elem_pp children)
+  ,if null children then "" else concat ["</", name, ">"]]
+
+picture_to_svg :: R -> FilePath -> Picture R -> IO ()
+picture_to_svg m fn p = writeFile fn (svg_elem_pp (picture_to_svg_elem m p))
