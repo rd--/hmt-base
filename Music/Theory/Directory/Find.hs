d501f1c1199c5ed44dbc49aac8e9a5d37cbedb98
rohan <rd@slavepianos.org>
2026-08-21 14:39:48 +1000

    nicerHelp

--- Music/Theory/Directory/Find.hs
+++ Music/Theory/Directory/Find.hs
@@ -1,22 +1,24 @@
 -- | Directory functions using 'find' system utility.
 module Music.Theory.Directory.Find where
 
-import Data.List {- base -}
-import Data.Maybe {- base -}
+import qualified Data.List {- base -}
+import qualified Data.Maybe {- base -}
 
 import qualified System.Process {- process -}
 
 {- | Find files having indicated filename.
 This runs the system utility /find/, so is Unix only.
 
-> dir_find "DX7-ROM1A.syx" "/home/rohan/sw/hsc3-data/data/yamaha/"
+>>> dir_find "DX7-ROM1A.syx" "/home/rohan/sw/hsc3-data/data/yamaha/"
+["/home/rohan/sw/hsc3-data/data/yamaha/dx7/rom/DX7-ROM1A.syx"]
 -}
 dir_find :: FilePath -> FilePath -> IO [FilePath]
 dir_find fn dir = fmap lines (System.Process.readProcess "find" [dir, "-name", fn] "")
 
 {- | Require that exactly one file is located, else error.
 
-> dir_find_1 "DX7-ROM1A.syx" "/home/rohan/sw/hsc3-data/data/yamaha/"
+>>> dir_find_1 "DX7-ROM1A.syx" "/home/rohan/sw/hsc3-data/data/yamaha/"
+"/home/rohan/sw/hsc3-data/data/yamaha/dx7/rom/DX7-ROM1A.syx"
 -}
 dir_find_1 :: FilePath -> FilePath -> IO FilePath
 dir_find_1 fn dir = do
@@ -28,25 +30,31 @@ dir_find_1 fn dir = do
 {- | Recursively find files having case-insensitive filename extension.
 This runs the system utility /find/, so is Unix only.
 
-> dir_find_ext ".syx" "/home/rohan/sw/hsc3-data/data/yamaha/"
+>>> r <- dir_find_ext ".syx" "/home/rohan/sw/hsc3-data/data/yamaha/"
+>>> length r
+220
 -}
 dir_find_ext :: String -> FilePath -> IO [FilePath]
 dir_find_ext ext dir = fmap lines (System.Process.readProcess "find" [dir, "-iname", '*' : ext] "")
 
 {- | Post-process 'dir_find_ext' to delete starting directory.
 
-> dir_find_ext_rel ".syx" "/home/rohan/sw/hsc3-data/data/yamaha/"
+>>> r <- dir_find_ext_rel ".syx" "/home/rohan/sw/hsc3-data/data/yamaha/"
+>>> length r
+220
 -}
 dir_find_ext_rel :: String -> FilePath -> IO [FilePath]
 dir_find_ext_rel ext dir =
-  let f = fromMaybe (error "dir_find_ext_rel?") . stripPrefix dir
+  let f = Data.Maybe.fromMaybe (error "dir_find_ext_rel?")
+          . Data.List.stripPrefix dir
   in fmap (map f) (dir_find_ext ext dir)
 
 {- | Scan each directory on path recursively for file.
 Stop once a file is located.
 Runs 'dir_find' so is Unix only.
 
-> path_scan_recursively ["/home/rohan/sw/hmt-base"] "Directory.hs"
+>>> path_scan_recursively ["/home/rohan/sw/hmt-base"] "Directory.hs"
+Just "/home/rohan/sw/hmt-base/Music/Theory/Directory.hs"
 -}
 path_scan_recursively :: [FilePath] -> FilePath -> IO (Maybe FilePath)
 path_scan_recursively p fn =
@@ -61,7 +69,9 @@ path_scan_recursively p fn =
 {- | Search each directory on path recursively for file.
 Runs 'dir_find' so is Unix only.
 
-> path_search_recursively ["/home/rohan/sw"] "README.md"
+>>> r <- path_search_recursively ["/home/rohan/sw"] "README.md"
+>>> length r
+172
 -}
 path_search_recursively :: [FilePath] -> FilePath -> IO [FilePath]
 path_search_recursively p fn =
